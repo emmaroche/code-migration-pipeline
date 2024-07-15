0b3115b457e8fbffa61bb0f8fc243b35fdb2c103
Emma R <20088680@mail.wit.ie>
2024-07-15 15:10:04 +0100

    Allow models to run at same time

--- code_migration_request.py
+++ code_migration_request.py
@@ -10,28 +10,43 @@ import time
 api_endpoint = 'http://127.0.0.1:5000/code-migration'
 
 # Define the source and target languages
-source_language = "Java"
-target_language = "Kotlin"
+source_language = 'Java'
+target_language = 'Kotlin'
 
 # Define file extension mappings for target languages
 language_extensions = {
-    "Kotlin": "kt",
-    "Python": "py",
-    "Swift": "swift",
-    "TypeScript": "ts",
+    'Kotlin': 'kt',
+    'Python': 'py',
+    'Swift': 'swift',
+    'TypeScript': 'ts',
 }
 
 # List of file paths to migrate from the repository
 repositories = [
     {
-        "repo": "emmaroche/data-preparation",
-        "file_paths": [
-            "code-artefacts/java/ShopV6.0/src/controllers/Store.java",
-            "code-artefacts/java/ShopV6.0/src/utils/Utilities.java",
-            "code-artefacts/java/ShopV6.0/src/utils/ScannerInput.java",
-            "code-artefacts/java/ShopV6.0/src/models/Product.java" 
+        'repo': 'emmaroche/data-preparation',
+        'file_paths': [
+            # 'code-artefacts/java/ShopV6.0/src/controllers/Store.java',
+            # 'code-artefacts/java/ShopV6.0/src/utils/Utilities.java',
+            # 'code-artefacts/java/ShopV6.0/src/utils/ScannerInput.java',
+            # 'code-artefacts/java/ShopV6.0/src/models/Product.java',
+            'code-artefacts/java/ShopV6.0/src/main/Driver.java'
         ]
     },
+]
+
+# List of models to run sequentially
+models = [
+    'VertexAI - PaLM 2',
+    'VertexAI - Gemini Pro',
+    # 'VertexAI - Codey',
+    # 'OpenAI - GPT-3.5 Turbo',
+    # 'OpenAI - GPT-4o',
+    # 'OpenAI - GPT-4 Turbo',
+    # 'Ollama - Llama 2',
+    # 'Ollama - Llama 3',
+    # 'Ollama - CodeGemma',
+    # 'Ollama - CodeLlama'
 ]
 
 # Function to extract folder name from file path and create new folder if needed
@@ -68,8 +83,8 @@ def extract_code_and_extra_content(response_json):
 # Function to extract code and extra content based on indentation
 def extract_code_and_extra_content_indentation(response_json):
     # Initialise variables
-    migrated_code = ""
-    extra_content = ""
+    migrated_code = ''
+    extra_content = ''
 
     # Check if 'migrated_code' exists in response_json
     if 'migrated_code' in response_json:
@@ -101,37 +116,41 @@ def extract_code_combined(response_json):
 
 # Map models to their respective extraction functions
 extraction_functions = {
-    "VertexAI - PaLM 2": extract_code_and_extra_content,  # Use regex-based extraction
-    "VertexAI - Gemini Pro": extract_code_combined,  # Use regex-based extraction
-    "VertexAI - Codey": extract_code_combined,  # Use regex-based extraction
-    "OpenAI - GPT-3.5 Turbo": extract_code_combined,  # Use regex-based extraction
-    "OpenAI - GPT-4o": extract_code_combined,  # Use regex-based extraction
-    "OpenAI - GPT-4 Turbo": extract_code_combined,  # Use regex-based extraction
-    "Ollama - Llama 2": extract_code_combined,  # Use indentation-based extraction
-    "Ollama - Llama 3":extract_code_combined,  # Use indentation-based extraction
-    "Ollama - CodeGemma": extract_code_combined,  # Use indentation-based extraction
-    "Ollama - CodeLlama": extract_code_combined,  # Use indentation-based extraction
+    'VertexAI - PaLM 2': extract_code_combined,
+    'VertexAI - Gemini Pro': extract_code_combined,  
+    'VertexAI - Codey': extract_code_combined, 
+    'OpenAI - GPT-3.5 Turbo': extract_code_combined, 
+    'OpenAI - GPT-4o': extract_code_combined, 
+    'OpenAI - GPT-4 Turbo': extract_code_combined,  
+    'Ollama - Llama 2': extract_code_combined, 
+    'Ollama - Llama 3': extract_code_combined,  
+    'Ollama - CodeGemma': extract_code_combined, 
+    'Ollama - CodeLlama': extract_code_combined
 }
 
 # Timer variables
 start_time = time.time()
 total_requests = 0
+model_times = {}
 
 # Function to handle code migration and saving the results
 def migrate_code(github_repo, github_file_path, selected_model, extraction_functions):
+        
+    model_start_time = time.time()
+    
     # Construct the raw URL
-    raw_url = f"https://raw.githubusercontent.com/{github_repo}/master/{github_file_path}"
+    raw_url = f'https://raw.githubusercontent.com/{github_repo}/master/{github_file_path}'
 
     # Download the file content from GitHub
     response = requests.get(raw_url)
     if response.status_code == 200:
         code_to_convert = response.text
     else:
-        raise Exception(f"Failed to download the file from GitHub ({github_repo}/{github_file_path}). Status code: {response.status_code}")
+        raise Exception(f'Failed to download the file from GitHub ({github_repo}/{github_file_path}). Status code: {response.status_code}')
 
     # Define the prompt and selected model name
     prompt = (
-        f"Migrate the provided {source_language} code to {target_language}, ensuring compatibility and functionality."
+        f'Migrate the provided {source_language} code to {target_language}, ensuring compatibility and functionality.'
     )
 
     # Request payload
@@ -146,14 +165,14 @@ def migrate_code(github_repo, github_file_path, selected_model, extraction_funct
 
     # Checking if the request was successful
     if response.status_code == 200:
-        print(f"Model Used for {github_repo}/{github_file_path}: ", selected_model)
+        print(f'Model Used for {github_repo}/{github_file_path}: ', selected_model)
         # Extract the response JSON
         response_json = response.json()
 
         # Get the extraction function based on the selected model
         extraction_function = extraction_functions.get(selected_model)
         if extraction_function is None:
-            raise ValueError(f"No extraction function found for model: {selected_model}")
+            raise ValueError(f'No extraction function found for model: {selected_model}')
 
         # Extract the migrated code and extra content from the JSON using the specified function
         migrated_code, extra_content = extraction_function(response_json)
@@ -163,7 +182,7 @@ def migrate_code(github_repo, github_file_path, selected_model, extraction_funct
         response_json['extra_content'] = extra_content
 
         # Log the migrated code for debugging purposes
-        print(f"Migrated code for {github_repo}/{github_file_path}:\n{migrated_code}")
+        print(f'Migrated code for {github_repo}/{github_file_path}:\n{migrated_code}')
 
         # Determine the output folder based on the file path
         folder_name = get_folder_name(github_file_path)
@@ -181,62 +200,72 @@ def migrate_code(github_repo, github_file_path, selected_model, extraction_funct
         target_language_extension = language_extensions.get(target_language, 'txt')
 
         # Generate output file path
-        output_file_path = os.path.join(output_folder, f"migrated_code_{target_language}_{unique_identifier}_{os.path.basename(github_file_path)}.{target_language_extension}")
+        output_file_path = os.path.join(output_folder, f'migrated_code_{target_language}_{unique_identifier}_{os.path.basename(github_file_path)}.{target_language_extension}')
 
         # Save the migrated code to the unique file
         if migrated_code:
             with open(output_file_path, 'w') as file:
                 file.write(migrated_code)
-            print(f"Migrated code for {github_repo}/{github_file_path} has been saved to {output_file_path}")
+            print(f'Migrated code for {github_repo}/{github_file_path} has been saved to {output_file_path}')
         else:
-            print(f"No valid migrated code for {github_repo}/{github_file_path} using model {selected_model}")
+            print(f'No valid migrated code for {github_repo}/{github_file_path} using model {selected_model}')
 
         # Save the entire response JSON to a new JSON file
         json_folder = os.path.join('output', 'json', selected_model.split(' - ')[-1])
         os.makedirs(json_folder, exist_ok=True)
-        json_file_path = os.path.join(json_folder, f"response_{os.path.basename(github_file_path)}_{unique_identifier}.json")
+        json_file_path = os.path.join(json_folder, f'response_{os.path.basename(github_file_path)}_{unique_identifier}.json')
         with open(json_file_path, 'w') as json_file:
             json.dump(response_json, json_file, indent=4)
 
-        print(f"Response JSON for {github_repo}/{github_file_path} has been saved to {json_file_path}")
+        print(f'Response JSON for {github_repo}/{github_file_path} has been saved to {json_file_path}')
 
         # SonarQube project key 
-        sonar_project_key = "Dissertation"
+        sonar_project_key = 'Dissertation'
 
         # Run SonarScanner to analyse the migrated code
         sonar_scanner_command = (
-            "sonar-scanner.bat "
-            f"-D\"sonar.projectKey={sonar_project_key}\" "
-            "-D\"sonar.sources=output\" "  
-            "-D\"sonar.host.url=http://localhost:9000\" "
-            "-D\"sonar.token=sqp_597c320197ea7108a85634755a2b3f8393afdb8e\""
+            'sonar-scanner.bat '
+            f'-D"sonar.projectKey={sonar_project_key}" '
+            '-D"sonar.sources=output" '  
+            '-D"sonar.host.url=http://localhost:9000" '
+            '-D"sonar.token=sqp_597c320197ea7108a85634755a2b3f8393afdb8e"'
         )
 
-        print("\nRunning SonarScanner...\n")
+        print('\nRunning SonarScanner...\n')
         subprocess.run(sonar_scanner_command, shell=True, check=True)
+
+        # Stop timer for the model
+        model_end_time = time.time()
+        model_time = model_end_time - model_start_time
+        model_times[selected_model] = model_times.get(selected_model, 0) + model_time
 
         return True
     else:
-        print(f"Error for {github_repo}/{github_file_path} using model {selected_model}: {response.text}")
+        print(f'Error for {github_repo}/{github_file_path} using model {selected_model}: {response.text}')
         return False
 
 # Iterate over each repository and file path
 for repo_info in repositories:
-    github_repo = repo_info["repo"]
-    for github_file_path in repo_info["file_paths"]:
-        # Define the model to use for this run
-        selected_model = "Ollama - Llama 2"  # Change the model name here as needed
-        extraction_function = extraction_functions.get(selected_model)
-        if extraction_function is None:
-            raise ValueError(f"No extraction function found for model: {selected_model}")
-        
-        if migrate_code(github_repo, github_file_path, selected_model, extraction_functions):
-            total_requests += 1
+    github_repo = repo_info['repo']
+    for github_file_path in repo_info['file_paths']:
+        # Iterate over each model
+        for selected_model in models:
+            extraction_function = extraction_functions.get(selected_model)
+            if extraction_function is None:
+                raise ValueError(f'No extraction function found for model: {selected_model}')
+            
+            if migrate_code(github_repo, github_file_path, selected_model, extraction_functions):
+                total_requests += 1
 
 # Calculate total time taken
 end_time = time.time()
 total_time = end_time - start_time
 
+# Print model times
+print("\nModel execution times:")
+for model, time_taken in model_times.items():
+    print(f"{model}: {time_taken:.2f} seconds")
+
 # Print summary
-print(f"\nAll requests completed in {total_time:.2f} seconds.")
-print(f"Total number of requests processed: {total_requests}")
+print(f'\nAll requests completed in {total_time:.2f} seconds.')
+print(f'Total number of requests processed: {total_requests}')
